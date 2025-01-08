c47e326b4245bc1f429010e7930f5760158031cd
alex.work.04.26@gmail.com <alex.work.04.26@gmail.com>
2025-01-08 12:27:44 +0300

    completed lesson №3

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,12 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+            int numberClient = 10;
+            double checkSum = 145.1;
+            boolean required = true;
+            String nameClient = "Name";
 
-        }
+            System.out.println(nameClient);
+
     }
 }
