bf740dfa69f5595f0e2a0b582c540f52dece16ee
alex.work.04.26@gmail.com <alex.work.04.26@gmail.com>
2025-01-08 16:33:29 +0300

    completed lesson №8

---
+++ src/Account.java
@@ -0,0 +1,61 @@
+import java.text.DecimalFormat;
+
+public class Account {
+
+    private String number;
+    private String owner;
+    private Double summa;
+
+    public Account(String number, String owner, Double summa) {
+        this.number = number;
+        this.owner = owner;
+        this.summa = summa;
+    }
+
+    public Account() {
+    }
+
+    public void checkStatus(){
+        System.out.println("Статус счёта");
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+
+    public String getOwner() {
+        return owner;
+    }
+
+    public void setOwner(String owner) {
+        this.owner = owner;
+    }
+
+    public Double getSumma() {
+        return summa;
+    }
+
+    public void setSumma(double summa) {
+        this.summa = summa;
+    }
+
+    public Boolean enoughMoney(Double moneySum){
+        String accountNumber = getNumber();
+        return (getSumma() - moneySum) >= 0;
+    }
+
+    public String withdrawMoney(Double moneySum){
+        boolean enoughMoney = enoughMoney(moneySum);
+
+        if (enoughMoney){
+            setSumma(getSumma() - moneySum);
+            return "С вашего счёта успешно снята сумма " + moneySum + ". Текущий баланс счёта: " + getSumma();
+        }else{
+            return "Недостаточно средств для снятия наличности с вашего счёта";
+        }
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -2,29 +2,48 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-            int numberClient = 10;
-            double checkSum = 145.1;
-            boolean required = true;
-            String nameClient = "Alex";
-            String sonameClient = "Smith";
-            boolean nameClientsStartWithN;
-            String formatMessage = String.format("Моё имя %s , сумма на моём счету = %.2f", (nameClient + " " + sonameClient), checkSum);
+//  Данные по предыдущим урокам
+//            int numberClient = 10;
+//            double checkSum = 145.1;
+//            boolean required = true;
+//            String nameClient = "Alex";
+//            String sonameClient = "Smith";
+//            boolean nameClientsStartWithN;
+//            String formatMessage = String.format("Моё имя %s , сумма на моём счету = %.2f", (nameClient + " " + sonameClient), checkSum);
+//
+//            System.out.println(nameClient);
+//
+//            nameClientsStartWithN = nameClient.startsWith("N");
+//
+//            System.out.println(nameClientsStartWithN);
+//
+//            String nameClientsUpper = nameClient.toUpperCase();
+//
+//            System.out.println(nameClientsUpper);
+//
+//            System.out.println( nameClient + " " + sonameClient + ". checkSum: " + (checkSum + 102.4));
+//
+//            System.out.println(formatMessage);
+//
+//            System.out.println(formatMessage.replaceAll(nameClient, "Pit"));
+            Double withdrawSub = 500.00;
 
-            System.out.println(nameClient);
+            Account account = new Account();
+            account.checkStatus();
+            account.setNumber("12343534523");
+            account.setSumma(1200.42);
 
-            nameClientsStartWithN = nameClient.startsWith("N");
+//            System.out.println(account.withdrawMoney(withdrawSub));
 
-            System.out.println(nameClientsStartWithN);
+            Account account2 = new Account();
+            account2.setNumber("5431123131");
+//            System.out.println(account2.getNumber());
 
-            String nameClientsUpper = nameClient.toUpperCase();
-
-            System.out.println(nameClientsUpper);
-
-            System.out.println( nameClient + " " + sonameClient + ". checkSum: " + (checkSum + 102.4));
-
-            System.out.println(formatMessage);
-
-            System.out.println(formatMessage.replaceAll(nameClient, "Pit"));
-
+            Account account3 = new Account("999999", "Alex", 150000.00);
+            System.out.println(account3.getNumber());
+            System.out.println(account3.getOwner());
+            System.out.println(account3.getSumma());
+            account3.withdrawMoney(withdrawSub);
+            System.out.println(account3.getSumma());
     }
 }
