13b881d58c40c4fb32faadf77f73c4108eac996a
alex.work.04.26@gmail.com <alex.work.04.26@gmail.com>
2025-01-14 22:04:17 +0300

    completed lesson№23

--- src/test/java/tests/SimpleTests.java
+++ src/test/java/tests/SimpleTests.java
@@ -1,8 +1,6 @@
 package tests;
 
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
 import java.util.Random;
 
@@ -11,17 +9,45 @@ public class SimpleTests {
     private Integer param1;
     private Integer param2;
 
-    @Test
-    @DisplayName("Сравнение двух раномно сгенерированных параметров")
-    public void testCompareTwoInteger() {
+
+    @BeforeEach
+    public void beforeTests(){
 
         Random random = new Random();
-
 
         param1 = random.nextInt(10);
         param2 = random.nextInt(10);
 
-        Assertions.assertTrue(param1<param2, "Тест не пройден, великий рандом не помог");
+    }
 
+    @Test
+    @DisplayName("Сравнение двух раномно сгенерированных параметров")
+    public void testCompareTwoInteger() {
+
+
+
+        int constant = 5;
+        int constant2 = 10;
+
+        int sum = constant2 + constant;
+
+        Assertions.assertTrue(param1<param2, "Тест не пройден, великий рандом не помог");
+        Assertions.assertEquals(15, sum, "Что-то пошло не так :-)");
+    }
+
+
+    @Test
+    @Disabled("Params")
+    @DisplayName("Тест для проверки строки")
+    public void testCompareString(){
+        String myString = "Какая то строчка";
+
+        Assertions.assertTrue(myString instanceof String);
+    }
+
+    @AfterEach
+    public void afterTests(){
+        System.out.println(param1);
+        System.out.println(param2);
     }
 }
