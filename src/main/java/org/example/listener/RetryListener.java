8f3feeb15c4458dd7e9d490e4c6d2a52df98bbff
alex.work.04.26@gmail.com <alex.work.04.26@gmail.com>
2025-02-16 16:09:49 +0300

    completed lesson №31

--- src/main/java/org/example/listener/RetryListener.java
+++ src/main/java/org/example/listener/RetryListener.java
@@ -2,9 +2,7 @@ package org.example.listener;
 
 import lombok.NoArgsConstructor;
 import org.apache.commons.io.FileUtils;
-import org.example.Settings;
 import org.example.yamlConfig.YamlConfig;
-import org.example.yamlConfig.YamlConfig.Config;
 import org.junit.jupiter.api.extension.AfterTestExecutionCallback;
 import org.junit.jupiter.api.extension.ExtensionContext;
 import org.junit.jupiter.api.extension.TestExecutionExceptionHandler;
@@ -23,26 +21,26 @@ public class RetryListener implements TestExecutionExceptionHandler, AfterTestEx
     private static final Set<String> failedTestNames = new HashSet<>();
 
     @Override
-    public void handleTestExecutionException(ExtensionContext extensionContext, Throwable throwable) throws Throwable {
+    public void handleTestExecutionException(ExtensionContext context, Throwable throwable) throws Throwable {
         for (int i = 0; i < MaxRetries; i++){
             try {
-                extensionContext.getRequiredTestMethod().invoke(extensionContext.getRequiredTestInstance());
+                context.getRequiredTestMethod().invoke(context.getRequiredTestInstance());
                 return;
             } catch (Throwable ex){
-                throwable = ex;
+                throwable = ex.getCause();
             }
         }
         throw throwable;
     }
 
     @Override
-    public void afterTestExecution(ExtensionContext extensionContext) throws Exception {
-        Method method = extensionContext.getRequiredTestMethod();
-        String testClass = extensionContext.getRequiredTestClass().getName();
+    public void afterTestExecution(ExtensionContext context) throws Exception {
+        Method method = context.getRequiredTestMethod();
+        String testClass = context.getRequiredTestClass().getName();
         String testName = method.getName();
-        String testToWrite = String.format("--test %s.%s*", testClass, testName);
+        String testToWrite = String.format("--tests %s.%s*", testClass, testName);
 
-        extensionContext.getExecutionException().ifPresent(x->failedTestNames.add(testToWrite));
+        context.getExecutionException().ifPresent(x->failedTestNames.add(testToWrite));
     }
 
     public static void saveResultToFile() throws IOException {
